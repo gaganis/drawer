17ba30eb1e1d30bb73b06b29de1a60f44cc0fd1f
Giorgos Gaganis <gaganis@yahoo.com>
2018-10-21 21:02:52 +0300

    Make compilable and readable stub

--- src/com/giorgosgaganis/mainwindow/MainDrawerWindow.java
+++ src/com/giorgosgaganis/mainwindow/MainDrawerWindow.java
@@ -3,18 +3,22 @@ package com.giorgosgaganis.mainwindow;
 import javax.swing.JComboBox;
 
 /*
-Our client wants us to draw shapes. To do this he has has given us this class where we are give 2 methods where we
-can register the types of shapes that we can draw and a second function that will be called when the mouse is clicked
-with the coordinates and the selected type.
+Our client wants us to draw shapes. To do this he has has given
+us this class where we are give 2 methods where we can register
+the types of shapes that we can draw and a second function that
+will be called when the mouse is clicked with the coordinates
+and the selected type.
 
 We are also given a DrawingJPanel that we can use to draw our shapes.
 
-* We need to make all the necessary changes so we can draw circles and a square. The size of the square is 100px and the
-radious of the circle is 80 pixes. We are also asked to display the area of the shape.
+* We need to make all the necessary changes so we can draw circles
+  and a square. The size of the square is 100px and the radius of the
+  circle is 80 pixes. We are also asked to display the area of the
+  shape.
 
-* Next we are asked to also draw a rectangle with height 50 and width 200.
-
-* Next we are asked to draw the
+* Next we are asked to also draw a rectangle with width 200 and
+  height 50.
+*/
 
  */
 public class MainDrawerWindow extends AbstractDrawerWindow {
